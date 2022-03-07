66772cc2193443369171d20701e43798bc8080c9
Obirijah Tochukwu <tobirijah@gmail.com>
2022-03-07 02:56:34 -1200

    mugu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,17 +7,19 @@ export default function Navbar() {
   window.location.hash = hash;
 
   return (
-    <div className='desktop container'>
-      <div className="navbar">
-        <h1 style={{textTransform:'uppercase', fontWeight: 'bolder', color: 'rgb(78, 8, 184)'}}>lunatix</h1>    
-        <span>
-          {links.map((link)=>{
-            const {id, url, text } = link;
-            return (
-              <a key={id} href={url} onClick={()=> setHash(url)} className={`navbar-link p-3 my-0 mx-1 ${hash === url && 'active'}`}>{text}</a>
-            )
-          })}
-        </span>
+    <div className='desktop'>
+      <div className="navbar bg-white">
+        <div className="container">
+          <h1 style={{textTransform:'uppercase', fontWeight: 'bolder', color: 'rgb(78, 8, 184)'}}>lunatix</h1>    
+          <span>
+            {links.map((link)=>{
+              const {id, url, text } = link;
+              return (
+                <a key={id} href={url} onClick={()=> setHash(url)} className={`navbar-link p-3 my-0 mx-1 ${hash === url && 'active'}`}>{text}</a>
+              )
+            })}
+          </span>
+        </div>
       </div>
     </div>
   )
