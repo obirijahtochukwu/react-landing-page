27673a6352919ec0a18c79d8881289d3f0b12a57
Obirijah Tochukwu <tobirijah@gmail.com>
2022-08-10 21:51:11 -1200

    'lo'

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
-import Ap from './Ap';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Ap />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
